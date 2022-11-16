1d1f823397353bea386fa6452b9664d1646b6dca
Megnate <1327358992@qq.com>
2022-11-16 22:25:02 +0800

    establish the first part

---
+++ README.md
@@ -0,0 +1,4 @@
+# This is a project that uses typescript vue3 nuxt3 vue-router scss to build an animated responsive side navigation bar with menus
+
+At the beginning, use a nuxt3 module that I [established before](https://github.com/Megnate/my-nuxt3-default-module) to build the basic part. 
+
---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts" name="app">
+import Todo from '@c/todo/Todo.vue'
+
+const runtimeConfigKey = useRuntimeConfig()
+const appConfigValue = useAppConfig()
+// const { curTime } = useGetTime()
+</script>
+
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    {{ runtimeConfigKey }}
+    {{ appConfigValue }}
+    <Todo />
+  </div>
+</template>
